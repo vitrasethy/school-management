1404216cd8cb0537c558efef40e8a11df8c161d7
vitrasethy <sethy.vitra@gmail.com>
2024-10-11 10:13:16 +0700

    change api from sanctum to passport oauth pkce.

---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Group;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Group */
+class GroupResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'school_year' => $this->school_year,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,12 @@
 <?php
 
-use App\Http\Controllers\Auth\Api\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\Api\RegisteredUserController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\SchoolController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', [UserController::class, 'getCurrentUser'])
-    ->middleware('auth:sanctum');
-
-Route::post('/register', [RegisteredUserController::class, 'store'])
-    ->middleware('guest')
-    ->name('register');
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
-
-Route::apiResource('/schools', SchoolController::class)->middleware('auth:sanctum');
-Route::apiResource('schools.departments', DepartmentController::class)
-    ->shallow()
-    ->middleware('auth:sanctum');
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [UserController::class, 'getCurrentUser']);
+    Route::apiResource('/schools', SchoolController::class);
+    Route::apiResource('schools.departments', DepartmentController::class)->shallow();
+});
