785ed19edb702d3ad3da52a5f7cb06e5655483c5
vitrasethy <sethy.vitra@gmail.com>
2024-11-20 13:48:47 +0700

    Prince Vegita

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -11,26 +11,12 @@ class SubjectSeeder extends Seeder
 {
     public function run(): void
     {
-        $teacher1 = User::factory()->create([
-            'role_id' => 4,
-            'school_id' => 1,
-            'department_id' => 1,
-        ]);
-
-        $teacher2 = User::factory()->create([
-            'role_id' => 4,
-            'school_id' => 1,
-            'department_id' => 1,
-        ]);
-
         $subject1 = Subject::create([
             'name' => 'Data Structures and Algorithms',
-            'teacher_id' => $teacher1->id,
         ]);
 
         $subject2 = Subject::create([
             'name' => 'Backend Developer',
-            'teacher_id' => $teacher2->id,
         ]);
 
         Group::find(1)->subjects()->attach([$subject1->id, $subject2->id]);
