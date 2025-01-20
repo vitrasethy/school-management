816eca544abc08deb0357cecd101484981ab0fc8
vitrasethy <sethy.vitra@gmail.com>
2025-01-20 16:36:58 +0700

    kamehameha

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -20,16 +21,23 @@ class UserSeeder extends Seeder
 
         User::factory(50)->create()->each(function (User $user) {
             $user->assignRole('student');
+
+            $facultyId = fake()->numberBetween(1, 10);
+            $departmentIds = Department::where('faculty_id', $facultyId)->pluck('id');
+
             $user->userAffiliations()->create([
-                'faculty_id' => fake()->numberBetween(1, 10),
-                'department_id' => fake()->numberBetween(1, 30),
+                'faculty_id' => $facultyId,
+                'department_id' => fake()->randomElement($departmentIds),
             ]);
         });
         User::factory(30)->create()->each(function (User $user) {
             $user->assignRole('teacher');
+            $facultyId = fake()->numberBetween(1, 10);
+            $departmentIds = Department::where('faculty_id', $facultyId)->pluck('id');
+
             $user->userAffiliations()->create([
-                'faculty_id' => fake()->numberBetween(1, 10),
-                'department_id' => fake()->numberBetween(1, 30),
+                'faculty_id' => $facultyId,
+                'department_id' => fake()->randomElement($departmentIds),
             ]);
         });
     }
