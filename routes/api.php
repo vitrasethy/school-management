02b270c472b64b82de2b4aa5328e1fb078ee7a39
vitrasethy <sethy.vitra@gmail.com>
2025-04-23 09:07:02 +0700

    kamehameha

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use App\Http\Controllers\Api\V2\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->prefix('v2')->group(function () {
-    Route::get('teacher/promotion', [TeacherController::class, 'promoteStudents']);
+    Route::post('teacher/promotion', [TeacherController::class, 'promoteStudents']);
 
     Route::get('teacher/dashboard', [TeacherController::class, 'dashboard']);
 
